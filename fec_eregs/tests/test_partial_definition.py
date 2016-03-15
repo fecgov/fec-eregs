759bf41c44a3872668eead0e736b9685ad94a1eb
Aaron D Borden <adborden@a14n.net>
2016-03-14 18:31:45 -0700

    wip: file-based mock api

--- fec_eregs/tests/test_partial_definition.py
+++ fec_eregs/tests/test_partial_definition.py
@@ -1,21 +1,17 @@
-from django.test import Client, TestCase
-from regcore.models import Regulation
 from bs4 import BeautifulSoup
-
+from django.test import TestCase
+import os.path
 
 class PartialDefinitionTests(TestCase):
+    def fixture_path(self, fixture_name='default'):
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+        return os.path.join(test_dir, 'fixtures', fixture_name) + os.sep
+
     def test_definition_with_ambiguous_paragraphs(self):
-        regulation = Regulation()
-        regulation.label_string = '100-5'
-        regulation.text = 'In this section, document means the following:'
-        regulation.title = 'A Regulation Title'
-        #regulation.children.append(Regulation())
-        regulation.save()
+        with self.settings(API_BASE=self.fixture_path('definition_with_ambiguous_paragraph')):
+            response = self.client.get('/partial/definition/100-5/2015-annual-100')
 
-        client = Client()
-        response = client.get('/partial/definition/100-5/2015-annual-100')
-
-        partial = BeautifulSoup(response.content)
-        self.assertTrue(partial.select('.definition-text p').text.contains('The first piece of this definition is:'))
-        self.assertTrue(partial.select('.definition-text p').text.contains('See the full definition at:'))
-        self.assertEqual(partial.select('.definition-text blockquote').text, regulation.text)
+            self.assertTemplateUsed('parital-definition.html')
+            self.assertContains(response, 'The first piece of this definition is:')
+            self.assertContains(response, 'See the full definition at:')
+            self.assertContains(response, 'Political committee means any group meeting one of the following conditions:')
