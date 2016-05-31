440ecb5a76acd3e2aadb668a3c6c0b6bd9c68a22
Aaron D Borden <adborden@a14n.net>
2016-05-31 15:44:43 -0700

    Env vars for other FEC app URLs

---
+++ fec_eregs/context_processors.py
@@ -0,0 +1,13 @@
+"""
+Defines Django context processors specific to fec_eregs.
+"""
+
+from django.conf import settings as settings
+
+
+def app_urls(request):
+    return {
+        'FEC_API_URL': settings.FEC_API_URL,
+        'FEC_CMS_URL': settings.FEC_CMS_URL,
+        'FEC_WEB_URL': settings.FEC_WEB_URL,
+    }
