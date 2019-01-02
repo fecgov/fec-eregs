36deb0a60a1ff21ad62bcf0941bf580d06fb3a03
qsun <qsun@loc.gov>
2019-01-02 10:15:53 -0500

    adding a docker-based loading script.

---
+++ load_regs/load_fec_regs_docker.py
@@ -0,0 +1,25 @@
+import sys
+from os import path
+from subprocess import call
+
+"""
+to run this script for eregs parsing and loading, you need:
+
+1. install docker on your machine
+2. make sure the eregs docker image is available on your machine
+3. make sure eregs.sh is available in same folder and executbile:
+   >> chmod +x eregs.sh
+
+refer this link for more details:
+https://eregs-parser.readthedocs.io/en/latest/installation.html
+"""
+
+with open('load_regs/fec_reg_parts.txt') as parts:
+    env = sys.argv[1].strip()
+    if env == 'local':
+        url = 'http://localhost:8000/api'
+    else:
+        url = "https://fec-{0}-eregs.app.cloud.gov/regulations/api".format(env)
+
+    for part in parts:
+        call(['./eregs.sh', 'pipeline', '11', part, url, '--only-latest'])
