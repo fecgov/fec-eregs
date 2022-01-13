65e1b91db131b9b0a1c20fbc8910692453325b58
John Carroll <jcarroll@macadmins-MacBook-Pro-4.local>
2022-01-13 13:17:12 -0500

    revert tasks

--- tasks.py
+++ tasks.py
@@ -60,7 +60,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'feature/eregs-639-mvp-ux'),
+    ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
